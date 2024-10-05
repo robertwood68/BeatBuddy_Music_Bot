f6d65d7527a02a417b22ce301937515ab881a004
Robert Wood <L.robertwood@gmail.com>
2024-10-05 11:48:07 -0400

    fixed docker-compose

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
    
 BeatBuddy is a Discord Music bot developed using Node.js.  At this time, BeatBuddy supports a wide variety of commands which can be found on its website https://beatbuddy.robertwood.dev or through inviting the bot to your server and typing "/" to pull up the newly developed slash commands!
 
-BeatBuddy is able to play music from multiple sources for all of the users in the voice channel to listen to.  As of late, the bot can play music based off of YouTube links and keyword searches, Spotify links, and SoundCloud links, all of which have playlist and podcast support too.  Just type "/play" and enter a keyword search or song link, and soon BeatBuddy will join the voice channel that you are in and begin to play the song or playlist that you requested.
+BeatBuddy is able to play music from multiple sources for all the users in the voice channel to listen to.  As of late, the bot can play music based off of YouTube links and keyword searches, Spotify links, and SoundCloud links, all of which have playlist and podcast support too.  Just type "/play" and enter a keyword search or song link, and soon BeatBuddy will join the voice channel that you are in and begin to play the song or playlist that you requested.
 
 The project originally started development with the intention of locally hosting it for a small number of people to use.  However, it has grown into a publicly hosted bot that is in plenty of servers, and I even developed a website for the bot for easy use, contacting me regarding issues with the bot, and more.
 
--- deploy-commands.js
+++ deploy-commands.js
@@ -1,42 +1,45 @@
 /**
- * Deploys any newly made slash commands.  
+ * Deploys commands from the list of slash commands when
+ * 'node deploy-commands.js' is run from the terminal
  * 
- * Enter node deploy-commands.js into the terminal to run.
- * 
- * @author Robert Wood 2023
+ * @author Rob Wood, 2024
  */
+// imports
+const { clientId, guildId, token } = require('./config.json');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
-const { clientId, guildId, token } = require('./config.json');
 const fs = require('node:fs');
 
+// empty array for commands
 const commands = [];
-// Grab all the command files from the commands directory you created earlier
+
+// grab each command file from the /commands directory
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-// Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
+// get the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
 
-// Construct and prepare an instance of the REST module
+// construct an instance of the REST module
 const rest = new REST({ version: '10' }).setToken(token);
 
-// and deploy your commands!
-(async () => {
-	try {
-		console.log(`Started refreshing ${commands.length} application (/) commands.`);
+// deploy the commands
+(
+	async () => {
+		try {
+			// log start
+			console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-		// The put method is used to fully refresh all commands in the guild with the current set
-		const data = await rest.put(
-            Routes.applicationCommands(clientId), //, guildId
-            { body: commands },
-        );
+			// refresh all the commands in the guild with the PUT method
+			const data = await rest.put(Routes.applicationCommands(clientId), { body: commands });
 
-		console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-	} catch (error) {
-		// And of course, make sure you catch and log any errors!
-		console.error(error);
+			// log end
+			console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+		} catch (error) {
+			// log errors
+			console.error(error);
+		}
 	}
-})();
+)();
